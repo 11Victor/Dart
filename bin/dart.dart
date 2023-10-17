ba72e29d830e886a256c97651b0083e50f4cbf86
Victor Valencio <victor.alcantara@unionitdigital.com.br>
2023-10-17 13:14:25 -0300

    feat: Inicializando projeto

---
+++ bin/dart.dart
@@ -0,0 +1,11 @@
+import 'package:dart/dart.dart' as dart;
+
+void main(List<String> arguments) {
+  int idade = 23;
+  double altura = 1.70;
+  bool geek = (idade==altura);
+
+  print('Hello world: ${dart.calculate()}!');
+  print(idade+altura);
+  print(geek);
+}
